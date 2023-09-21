9b82aeeb528aac280f149b22f56bae5e4cbaed30
LEHANS <latheeflathuthekkil@gmail.com>
2023-09-21 21:30:39 +0530

    fix:env

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "351f8f4a2fe4ca2e"; }
+/******/ 		__webpack_require__.h = function() { return "92de5ac260262be1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
